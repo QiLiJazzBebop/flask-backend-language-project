116b0924a6e23974240646c2ac6ad2f4450f6bff
QiLiJazzBebop <lql4@student.unimelb.edu.au>
2022-07-25 13:24:30 +1000

    fix error exception(word not found) bug

--- route/nodesBilingualBackwardGraph.py
+++ route/nodesBilingualBackwardGraph.py
@@ -6,17 +6,9 @@ convertRoute = APIRouter(prefix="/api/search/nodes/backward")
 
 @convertRoute.get("/en_jp/{wordGet}")
 def bilingualEnConvertGet(wordGet):
-    try:
-        res = bilingualNodes('en', 'jp', wordGet, False)
-        return res
-    except Exception as e:
-        return e
+    return bilingualNodes('en', 'jp', wordGet, False)
 
 
 @convertRoute.get("/jp_en/{wordGet}")
 def bilingualJpConvertGet(wordGet):
-    try:
-        res = bilingualNodes('jp', 'en', wordGet, False)
-        return res
-    except Exception as e:
-        return e
+    return bilingualNodes('jp', 'en', wordGet, False)
--- route/nodesBilingualForwardGraph.py
+++ route/nodesBilingualForwardGraph.py
@@ -6,17 +6,9 @@ convertRoute = APIRouter(prefix="/api/search/nodes/forward")
 
 @convertRoute.get("/en_jp/{wordGet}")
 def bilingualEnConvertGet(wordGet):
-    try:
-        res = bilingualNodes('en', 'jp', wordGet, True)
-        return res
-    except Exception as e:
-        return e
+    return bilingualNodes('en', 'jp', wordGet, True)
 
 
 @convertRoute.get("/jp_en/{wordGet}")
 def bilingualJpConvertGet(wordGet):
-    try:
-        res = bilingualNodes('jp', 'en', wordGet, True)
-        return res
-    except Exception as e:
-        return e
+    return bilingualNodes('jp', 'en', wordGet, True)
